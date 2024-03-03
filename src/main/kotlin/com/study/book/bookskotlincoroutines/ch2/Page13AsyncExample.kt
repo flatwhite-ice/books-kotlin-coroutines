bebf9d1516c79a409f78cb8c4f030c4574849742
flatwhite <flatwhite@flatwhiteui-MacBookAir.local>
2024-03-03 20:12:04 +0900

    alignment

--- src/main/kotlin/com/study/book/bookskotlincoroutines/ch2/Page13AsyncExample.kt
+++ src/main/kotlin/com/study/book/bookskotlincoroutines/ch2/Page13AsyncExample.kt
@@ -9,27 +9,27 @@ class Page13AsyncExample {
 
 }
 
-suspend fun delayedReturn(seed: Int): List<Triple<Int, String, Long>> {
+suspend fun delayedResponseFromExternService(serviceName: String): List<Triple<String, String, Long>> {
     val delayTime = Random.nextLong(3000L)
     delay(delayTime)
     val value = Random.nextInt(10).toString().also {
-        println("[${Thread.currentThread().name}][$seed] returnValue : $it (delayTime : $delayTime)")
+        println("[${Thread.currentThread().name}][service-name : $serviceName] response value : $it (delayTime : $delayTime)")
     }
-    return listOf(Triple(seed, value, delayTime))
+    return listOf(Triple(serviceName, value, delayTime))
 }
 
 fun main(args: Array<String>) {
 
+    val externServices = arrayListOf("wallet", "retail", "point", "stock", "payment", "admin")
+
     println("[${Thread.currentThread().name}]start.")
     runBlocking {
         println("[${Thread.currentThread().name}]run blocking start.")
-        val allNews = (0 until 10)
-            .map { seed -> async {
-                delayedReturn(seed)
-            } }
+        val allOf = externServices.indices
+            .map { seed -> async { delayedResponseFromExternService(externServices[seed]) } }
             .flatMap { it.await() }
 
-        println("[${Thread.currentThread().name}]allNews: ${allNews.sortedBy { it.third }}")
+        println("[${Thread.currentThread().name}]all : ${allOf.sortedBy { it.third }}")
         println("[${Thread.currentThread().name}]run blocking end.")
     }
     println("[${Thread.currentThread().name}]end.")
